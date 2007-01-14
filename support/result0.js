3760df29b1492c9c075e9d9a48cd699d24afaab4
Wakaba <w@suika.fam.cx>
2007-01-14 12:22:52 +0000

    New

---
+++ support/result0.js
@@ -0,0 +1,19 @@
+function htescape (s) {
+  return s.replace ('&', '&amp;').replace ('<', '&lt;');
+} // htescape
+
+function writeResult (value) {
+  if (typeof (value) == 'undefined') {
+    document.write ('<code>undefined</code>');
+  } else if (value == null) {
+    document.write ('<code>null</code>');
+  } else {
+    var vString = value.toString ();
+    if (vString == '') {
+      document.write ('An empty string');
+    } else {
+      document.write ('<code>' + htescape (vString) + '</code>');
+    }
+    document.write (', type <code>' + htescape (typeof (value)) + '</code>');
+  }
+} // writeResult
