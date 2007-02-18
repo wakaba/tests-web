e4415f2212eebfae95dfdceb247b067a45fec750
Wakaba <w@suika.fam.cx>
2007-02-18 04:54:39 +0000

    setValueResult: New function

--- support/result0.js
+++ support/result0.js
@@ -25,3 +25,31 @@ function writeResult (value) {
     document.write (', type <code>' + htescape (typeof (value)) + '</code>');
   }
 } // writeResult
+
+function setValueResult (obj, value) {
+  var rvalue = '';
+  if (typeof (value) == 'undefined') {
+    rvalue = 'undefined';
+  } else if (value == null) {
+    rvalue = 'null';
+  } else if (value.toString) {
+    var vString = value.toString ();
+    if (vString == '') {
+      rvalue = 'An empty string';
+    } else {
+      rvalue = vString.replace (/%/, '%25').replace (/\u000D/, '%0D')
+          .replace (/\u000A/, '%0A').replace (/\u000C/, '%0C');
+    }
+    rvalue += ', type ' + typeof (value);
+  } else {
+    var vString = '' + value;
+    if (vString == '') {
+      rvalue = 'An empty string';
+    } else {
+      rvalue = vString.replace (/%/, '%25').replace (/\u000D/, '%0D')
+          .replace (/\u000A/, '%0A').replace (/\u000C/, '%0C');
+    }
+    rvalue += ', type ' + typeof (value);
+  }
+  obj.value = rvalue;
+} // setValueResult
