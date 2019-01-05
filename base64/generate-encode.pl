a05b346c39298fecdf99563715f376f9a1617ffc
Wakaba <wakaba@suikawiki.org>
2019-01-05 15:03:10 +0900

    New base64 tests and base64url tests

--- base64/generate-encode.pl
+++ base64/generate-encode.pl
@@ -11,11 +11,14 @@ sub e ($$) {
 
 my $Bytes = [0..255];
 
+my $base64url = (shift // '') eq 'base64url';
+
 e '' => '';
 for (@$Bytes) {
   my $b1 = pack 'C', $_;
   my $bytes = join '', $b1;
   my $encoded = encode_base64 $bytes, '';
+  $encoded =~ tr{+/=}{-_}d if $base64url;
   e $bytes => $encoded;
 }
 for (@$Bytes) {
@@ -24,6 +27,7 @@ for (@$Bytes) {
     my $b2 = pack 'C', $_;
     my $bytes = join '', $b1, $b2;
     my $encoded = encode_base64 $bytes, '';
+    $encoded =~ tr{+/=}{-_}d if $base64url;
     e $bytes => $encoded;
   }
 }
@@ -33,6 +37,7 @@ for my $length (100, 255, 256, 1024, 1025, 4096, 4097) {
   my $bytes = '';
   $bytes .= $Bytes->[rand @$Bytes] for 0..$length;
   my $encoded = encode_base64 $bytes, '';
+  $encoded =~ tr{+/=}{-_}d if $base64url;
   e $bytes => $encoded;
 }
 
